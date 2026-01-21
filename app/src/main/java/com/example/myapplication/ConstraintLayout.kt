72b62e7e3a969670f8afdd170a3d1b0a07870155
Rajneesh Kumar <goldberg9087@gmail.com>
2026-01-22 00:27:45 +0530

    new assignment changes

--- app/src/main/java/com/example/myapplication/ConstraintLayout.kt
+++ app/src/main/java/com/example/myapplication/ConstraintLayout.kt
@@ -1,18 +1,16 @@
 package com.example.myapplication
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class RelativeLayout : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+class ConstraintLayout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.profile_screen)
+        setContentView(R.layout.constraint_layout)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
